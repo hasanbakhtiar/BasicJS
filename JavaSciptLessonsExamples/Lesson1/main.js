f82cbf51d1e8dbc0106a158aee1144f8fa4b767e
Hasan <hasan.doctype@gmail.com>
2021-04-18 12:32:18 +0400

    add JS Lesson Excample Part1

---
+++ JavaSciptLessonsExamples/Lesson1/main.js
@@ -0,0 +1,204 @@
+// ======================================Start===================================
+// =======================================End====================================
+
+// ======================================Start==========================================
+
+/*
+Çox sətrli comment
+
+*/
+
+// Tək sətrli comment
+// =======================================End====================================
+
+// ======================================Start==========================================
+// alert("Hi");
+// window.alert("Hi1");
+// =======================================End====================================
+
+// ======================================Start===================================
+// document.write("HI Javascript");
+// document.writeln("HI Javascript");
+// =======================================End====================================
+
+// ======================================Start===================================
+// console.log("HI JavaScript");
+// =======================================End====================================
+
+// ======================================Start===================================
+// document.write("Hi JavaScript1"+ "<br>");
+// debugger;
+// document.write("Hi JavaScript2"+ "<br>");
+// debugger;
+// document.write("Hi JavaScript3"+ "<br>");
+// debugger;
+// document.write("Hi JavaScript4"+ "<br>");
+// debugger;
+// document.write("Hi JavaScript5"+ "<br>");
+// =======================================End====================================
+
+
+// ======================================Start===================================
+// Data type (Data tipi)
+//   1.Bəsit(Primitive) data tipləri (String (mətn), Number (sayı), Boolean (mentiqi))
+//   2.Birləşik(Composite) data tipləri (Array (qovluq), Object (obyekt))
+//   3.Xüsusi(Special) data tipləri (Undefined (tanimlanmamis), Null (boş))
+
+// 1.Bəsit(Primitive) Data Types
+// // String (mətn)Data Type
+// const infoDataString = "Hi JavaScript";
+// const infoDataString1 = 'Hi JavaScript1';
+// document.write(infoDataString+"<br>");
+// document.write(infoDataString1+"<br>");
+
+// Number (say)Data Type
+// const infoDataNumb = 5;
+// const infoDataNumb1 = 10;
+// document.write(infoDataNumb+infoDataNumb1 +"<br>");
+
+// // Boolean (mentiqi)Data Type
+
+
+
+// 2.Birləşik(Composite) Data Types birdən çox məlumatı bir dəyişkənin içində saxlamaq üçün istifadə olunur.
+// Array Data Type
+// const infoDataBool = true; //false
+// document.write(infoDataBool);
+// const infoArray = [5,"hi",true];
+// document.write(infoArray);
+
+// Object Data Type
+// const infoOject = {
+//     name: "Muhammad",
+//     age: 24,
+//     address: "Baku",
+//     male: true
+// }
+// document.write(infoOject.name+"<br>"+infoOject.age+"<br>"+infoOject.address+"<br>"+infoOject.male)
+
+
+// 3.Xüsusi(Special) Data Types
+// const infoNull = null + "<br>";
+// document.write(infoNull);
+
+// const infoUndefined = undefined;
+// document.write(infoUndefined);
+
+
+// ======================================End===================================
+
+// ======================================Start===================================
+// Assignment Operators (Daxil Etmə operatorları)
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// var infoNumb = 5;
+// var infoNumb1 = 10;
+// // infoNumb = infoNumb + infoNumb1; 
+// // infoNumb += infoNumb1;
+// // infoNumb -= infoNumb1;
+// // infoNumb *= infoNumb1;
+// // infoNumb /= infoNumb1;
+// // infoNumb **= infoNumb1;
+
+// document.write(infoNumb);
+
+// Arithmetic Operators (Riyazi operatorlar) Start
+// +    Toplama
+// -    Çıxma
+// *    Vurma
+// /    Bölmə
+// %    Qalıq
+// **   Üstü
+// ++   Bir artırma
+// --   Bir əksiltmə
+
+// var infoNumb = 5;
+// infoNumb++;
+// var infoNumb1 = 10;
+// infoNumb1--;
+
+// document.write(infoNumb1);
+
+// --------------------------------------------------------------------------------------------------
+// Comparison Operators(Qarşılaşdırma operatorları) Start
+// ==   (qoşa bərabər)
+// ===  (üçlü bərabər)
+// !=   (nida tək bərabər)
+// !==  (nida qoşa bərabər)
+// <    (kiçikdir)
+// <=   (kiçik bərabərdir)
+// >    (böyükdür)
+// >=   (böyük bərabərdir)
+// var infoNumb  = 5;
+// var infoNumb1 = 10;
+
+
+// if (infoNumb >= infoNumb1) { //true
+//     document.write("düz ");
+// }else{
+//     document.write("səhv");
+
+// }
+
+
+// --------------------------------------------------------------------------------------------------
+// Logical Operators (Məntiqi operatorlar) Start
+// && (və) true && true = true # true && false = false # true && true && false = false # true && true && true = true 
+// || (və ya) true || true = true # true || false = true # true || false || false = true # true || true || true = true 
+// ! (deyil)  !(true) = false # !(false) = true
+
+
+// if (true && true) {
+//     document.write(true);
+// }else{
+//     document.write(false);
+// }
+
+// --------------------------------------------------------------------------------------------------
+// String  Operators (Mətn operatorları) Start
+// +    (toplama string data tiplərini birləşdirir)
+// +=   (toplama bərabərdir)
+// var infoText = "Hi";
+// var infoText1 = " JavaSscript";
+// infoText += infoText1;
+// document.write(infoText);
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodalrı bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// \            (tərs slash)(Qaçındırma operatorudur)
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// delete       (objectin dəyərini silmək üçündür)
+// new          (yeni dəyər yaratməq üçündür)
+// this         (mövcud dəyəri işarələmək üçündür)
+// in           (dəyişkənin daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+// const infoType = 5;
+// const infoType1 = 10;
+// infoType < infoType1 ? document.write("düz") : document.write("səhv") ;
+
+
+
+
+
+
+
+
+
+
+
+
