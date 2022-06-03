3cc0cde23da468181e618f192950077000762cd3
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-06-03 20:31:33 +0400

    module more

--- modulJS/js/main.js
+++ modulJS/js/main.js
@@ -1,4 +1,20 @@
-import { infonumb } from "./utils";
+// Chepter 1=====================================
+// import { infonumb, infoText } from "./utils.js";
+// console.log(infonumb(5));
+// console.log(infoText);
+// Chepter 1=====================================
 
 
-console.log(infonumb(5));
+// Chepter 2=====================================
+// import {  infonumb as a, infoText } from "./utils.js";
+// console.log(a(5));
+// console.log(infoText);
+// Chepter 2=====================================
+
+// Chepter 3=====================================
+// import * as Utils  from "./utils.js";
+// console.log(Utils.infonumb(5));
+// console.log(Utils.infoText);
+// Chepter 3=====================================
+
+
--- modulJS/js/utils.js
+++ modulJS/js/utils.js
@@ -1,4 +1,19 @@
-export function infonumb(n) {
-    return n * 2;
+// Chepter 1,2,3=====================================
+// export function infonumb(n) {
+//     return n * 2;
     
-}
+// }
+// export const infoText = 'Hello';
+
+
+// function infonumb(n) {
+//     return n * 2;
+    
+// }
+// const infoText = 'Hello';
+
+// export {infonumb, infoText};
+// Chepter 1,2,3=====================================
+
+
+
